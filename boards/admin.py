0b993bd9fb0040292491d8d06cce0a284c8dffe2
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-07 05:52:06 +0900

    Feat: 게시글 작성 및 조회 기능 구현

---
+++ boards/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post, PostImage
+
+class PostImageInline(admin.TabularInline): # 인라인으로 이미지 편집
+    model = PostImage
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline]
+    list_display = ('title', 'content', 'created_at')
+    list_filter = ('category',)  # 카테고리로 필터링
---
+++ boards/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from .models import Post, PostImage
+
+class PostImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = PostImage
+        fields = ['image']
+
+class PostSerializer(serializers.ModelSerializer):
+    images = PostImageSerializer(many=True, read_only=True)  # 다중 이미지 필드 허용
+    category = serializers.CharField(source='category')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def validate_images(self, value):
+        max_images = 2  # 이미지 수 2개로 제한
+        if len(value) > max_images:
+            raise serializers.ValidationError(f'최대 {max_images}개의 이미지를 업로드할 수 있습니다.')
+        return value
+
+    def create(self, validated_data):
+        post = Post.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            PostImage.objects.create(post=post, image=image_data)
+        return post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PostCreateSeraizlier(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = 'title', 'content', 'image'
+    
+
+class PostUpdateSerailizer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = 'title', 'content', 'image'
+
+class PostRetrieveSeraizlier(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        depth = 1
